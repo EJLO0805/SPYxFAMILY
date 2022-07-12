710a8606e3fa429a685dc75047b27ea6d91054f7
EJ <lyc7485@gmail.com>
2022-07-12 14:52:07 +0800

    Initial Commit

---
+++ SPYxFAMILY/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SPYxFAMILY
+//
+//  Created by 羅以捷 on 2022/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        enter.layer.cornerRadius = 40
+    }
+
+    @IBOutlet weak var enter: UIButton!
+    
+}
+
